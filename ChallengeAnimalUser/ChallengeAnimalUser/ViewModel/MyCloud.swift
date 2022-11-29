a89190e7c529b8234e061df329637c825a63fca6
Moyses Azevedo <m.mirazev@gmail.com>
2022-11-28 22:31:43 -0300

    refactory myCloud

--- ChallengeAnimalUser/ChallengeAnimalUser/ViewModel/MyCloud.swift
+++ ChallengeAnimalUser/ChallengeAnimalUser/ViewModel/MyCloud.swift
@@ -10,28 +10,33 @@ import CloudKit
 
 class MyCloud {
     var cache: ObservableObject<[CKRecord]?> = ObservableObject(nil)
+    var cache1: [CKRecord] = []
 //    cache tem que ser um modelo que receba todos os dados $0.0, record
     let publishContainer = CKContainer(identifier: "iCloud.Mirazev.AnimalUser").publicCloudDatabase
 
     func filterRecords(recordType: RecordsNamesEnum, dataBase: CKDatabase, filter: String = "") {
         var predicate = NSPredicate(value: true)
         if filter != "" { predicate = NSPredicate(format: filter)}
-
         let query = CKQuery(recordType: recordType.rawValue, predicate: predicate)
-        dataBase.fetch(withQuery: query) { records in
-            switch records {
+        dataBase.fetch(withQuery: query) { response in
+            switch response {
             case .success(let value):
-            var _ = value.matchResults.map({
+                _ = value.matchResults.map {
                     switch $0.1 {
                     case .success(let record):
-                        self.cache.value?.append(record)
+                        self.cache1.append(record)
                     case .failure(let error):
                         print(error)
                     }
-                })
+                }
             case .failure(let error):
                 print(error)
             }
+            self.cache.value = self.cache1
+            //Hipotese Python quando um map era feito ele nao era carregado ate que fosse transformado em uma lista
+            // Observei que quando uma instancia e usada nesse processo ela nao e carregada, Entretando
+            // Eu usei um for para confirmar e houve o mesmo problema, acredito ter relacao com o .sucess
         }
+
     }
 }
