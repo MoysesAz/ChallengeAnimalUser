5377f02f3782eb9f747be014d08690982649da53
Joan Wilson <joanwmoliveira@gmail.com>
2022-12-06 11:22:54 -0300

    refactor: refactoring the Pet View, ViewController and its ViewModel

--- ChallengeAnimalUser/ChallengeAnimalUser/Presentation/Pet/ViewModel/PetViewModel.swift
+++ ChallengeAnimalUser/ChallengeAnimalUser/Presentation/Pet/ViewModel/PetViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by Moyses Miranda do Vale Azevedo on 28/11/22.
 //
 
-import Foundation
+import UIKit
 import CloudKit
 
 class PetViewModel {
@@ -15,6 +15,29 @@ class PetViewModel {
     init(shelterId: CKRecord.Reference, titleView: String) {
         self.shelterId = shelterId
         self.titleView = titleView
+    }
+
+    public func makeCell(_ tableView: UITableView, cellForRowAt indexPath: IndexPath, records: [CKRecord]) -> UITableViewCell? {
+
+        guard
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: PetTableViewCell.identifier,
+                for: indexPath
+            ) as? PetTableViewCell,
+            let image: CKAsset = records[indexPath.row].object(forKey: "image") as? CKAsset,
+            let neutered = records[indexPath.row].value(forKey: "neutered") as? String,
+            let name = records[indexPath.row].value(forKey: "nameAnimal") as? String else {
+            return nil
+        }
+
+        let isNeureted = neutered == "true" ? true : false
+
+        let imageURL: URL? = URL(string: image.fileURL!.absoluteString)
+        let cellInfo = PetModel(name: name, image: imageURL, age: 10, isNeutered: isNeureted)
+
+        cell.petInfo = cellInfo
+
+        return cell
     }
 
 }
